fb9ddf5fc872d1d39ccd02bd566c4c447864f558
Daniel Kanangila <daniel.kanangila@gmail.com>
2020-04-18 11:54:22 -0400

    added user validation

--- users/userRouter.js
+++ users/userRouter.js
@@ -3,8 +3,19 @@ const User = require("./userDb");
 
 const router = express.Router();
 
-router.post('/', async (req, res) => {
+router.post('/', validateUser, async (req, res) => {
   // do your magic!
+  try {
+    const user = await User.insert(req.body);
+    if (!user) {
+      res.status(400).json({message: "Can't save user information"});
+      return false;
+    }
+    res.status(201).json(user);
+  } catch(err) {
+    console.error(err);
+    res.status(500).json({message: "An error occurred while trying to save new user."});
+  }
 });
 
 router.post('/:id/posts', validateUserId, async (req, res) => {
@@ -32,8 +43,19 @@ router.get('/:id/posts', validateUserId, (req, res) => {
   // do your magic!
 });
 
-router.delete('/:id', validateUser, (req, res) => {
+router.delete('/:id', validateUser, async (req, res) => {
   // do your magic!
+  try {
+    const user = await User.remove(req.params.body);
+    if (!user) {
+      res.send(400).json({message: "Can't delete user wth the specified ID"});
+      return false
+    }
+    res.status(200).json({id: req.params.id});
+  } catch(err) {
+    console.error(err);
+    res.status(500).json({message: "An error occurred while trying to retrieve  users."})
+  }
 });
 
 router.put('/:id', validateUserId, (req, res) => {
@@ -59,6 +81,15 @@ async function validateUserId(req, res, next) {
 
 function validateUser(req, res, next) {
   // do your magic!
+  if (!req.body) {
+    res.status(400).json({message: "missing user data"});
+    return false;
+  }
+  if (req.body && !req.body.name) {
+    res.status(400).json({message: "missing required name field"});
+    return false;
+  }
+  next();
 }
 
 function validatePost(req, res, next) {
