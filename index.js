cc678373c805b0246065886bdaa142f93ea036e3
Daniel Kanangila <daniel.kanangila@gmail.com>
2020-04-18 13:24:13 -0400

    updated server start script

--- index.js
+++ index.js
@@ -4,8 +4,6 @@ const postRouter = require("./posts/postRouter");
 const userRouter = require("./users/userRouter");
 const logger = require("./middleware/logger");
 
-const env = process.env;
-
 const app = express()
 // setting logger middleware
 app.use(logger)
@@ -13,6 +11,4 @@ app.use(express.json());
 app.use("/api/posts", postRouter);
 app.use("/api/users", userRouter);
 
-app.listen(env.PORT, () => {
-    console.log(`Application listen on ${env.HOST}:${env.PORT}`);
-});
+module.exports = app;
