1cd483d6a1ce475d5f512773ab5c6c04b9aedc6b
Daniel Kanangila <daniel.kanangila@gmail.com>
2020-04-18 01:31:58 -0400

    added logger middleware

--- index.js
+++ index.js
@@ -2,10 +2,13 @@
 const express = require("express");
 const postRouter = require("./posts/postRouter");
 const userRouter = require("./users/userRouter");
+const logger = require("./middleware/logger");
 
 const env = process.env;
 
 const app = express()
+// setting logger middleware
+app.use(logger)
 app.use(express.json());
 app.use("/api/posts", postRouter);
 app.use("/api/users", userRouter);
