38b4629b2ab496076f80a5b67e81b0f54153dc4c
Daniel Kanangila <daniel.kanangila@gmail.com>
2020-04-18 01:15:04 -0400

    First commit

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // code away!
+const express = require("express");
+const postRouter = require("./posts/postRouter");
+const userRouter = require("./users/userRouter");
+
+const env = process.env;
+
+const app = express()
+app.use(express.json());
+app.use("/api/posts", postRouter);
+app.use("/api/users", userRouter);
+
+app.listen(env.PORT, () => {
+    console.log(`Application listen on ${env.HOST}:${env.PORT}`);
+});
